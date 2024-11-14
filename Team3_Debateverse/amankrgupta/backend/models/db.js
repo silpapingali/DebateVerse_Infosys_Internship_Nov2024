0edabf6861d9fed0eb12543a3796a8bae2628591
Aman Kumar Gupta <amangupta65734@gmail.com>
2024-11-14 15:05:31 +0530

    updated

--- Team3_Debateverse/amankrgupta/backend/models/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const db=()=>{
-  mongoose.connect(process.env.DATABASE_URL).then(()=>{
-    console.log("database connected")
-  }).catch((err)=>console.log("db connection failed",err))
-}
-
-module.exports = db;
