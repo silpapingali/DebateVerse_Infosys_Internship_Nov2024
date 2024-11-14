84e6847d906464c3c6b7fdcf8d1bcbb9e3f9249a
Aman Kumar Gupta <amangupta65734@gmail.com>
2024-11-14 17:32:23 +0530

    updated by aman

--- Team3_Debateverse/amankrgupta/backend/routes/authRoutes.js
+++ Team3_Debateverse/amankrgupta/backend/routes/authRoutes.js
@@ -3,9 +3,10 @@ const express = require('express');
 const Authroute = express.Router();
 const { Validate } = require('../middlewares/AuthValidation');
 
-const {Login, Register} = require('../controllers/Auth')
+const {Login, Register} = require('../controllers/Auth');
+const { validate } = require('../models/userModels');
 
 Authroute.post('/login',Login)
-Authroute.post('/register', Register);
+Authroute.post('/register',Validate, Register);
 
 module.exports = Authroute;
