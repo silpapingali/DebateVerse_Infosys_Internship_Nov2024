8a9274684ef6bd7dcd456bc802ea978d12df7247
MSanjana-12 <sanjanamittapally12@gmail.com>
2024-11-19 22:30:31 +0530

    first commit

---
+++ Team3_Debateverse/Sanjana/frontend/src/pages/Reset.jsx
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { toast } from "react-toastify"
+import {Eye, EyeOff} from "lucide-react";
+
+export const Reset = () => {
+  const navigate = useNavigate();
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [params]= useSearchParams();
+  const token= params.get("token");
+  console.log(token);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match !");
+      return;
+    }
+    setLoading(true);
+    try {
+      const res = await axios.post(
+        `http://localhost:3000/api/auth/resetpassword`,
+        { token, password }
+      );
+      toast.success(res.data.message);
+      navigate("/login");
+    } catch (err) {
+      toast.error(err.response?.data?.message || "Error! Please try again");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-md">
+        <h2 className="text-2xl font-bold text-center">Create New Password</h2>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              New Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="confirmPassword"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Confirm Password
+            </label>
+            <input
+              id="confirmPassword"
+              name="confirmPassword"
+              type="text"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
+              required
+              className="w-full px-3 py-2 mt-1 border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className={`w-full px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm focus:outline-none ${
+                loading ? "bg-indigo-400" : "bg-indigo-600 hover:bg-indigo-700"
+              }`}
+              disabled={loading}
+            >
+              {loading ? "Resetting..." : "Reset Password"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Reset;
