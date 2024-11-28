8d05065449873ec39cad9c636700f9d052ea3d84
SameerSethi <sameersethi2003@gmail.com>
2024-11-28 18:17:03 +0530

    Add files via upload

--- Team4_Debateverse/Sameer/server/index.js
+++ Team4_Debateverse/Sameer/server/index.js
@@ -5,13 +5,14 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const UserModel = require("./models/employee");
+const nodemailer = require("nodemailer");
 
 const app = express();
 app.use(express.json());
 app.use(
   cors({
     origin: ["http://localhost:5173"],
-    methods: ["GET","POST"],
+    methods: ["GET", "POST"],
     credentials: true,
   })
 );
@@ -19,6 +20,29 @@ app.use(cookieParser());
 
 mongoose.connect("mongodb://localhost:27017/employee");
 
+const verifyUser = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json("token is missing");
+  } else {
+    jwt.verify(token, "jwt-secret-key", (err, decoded) => {
+      if (err) {
+        console.error("JWT Verification Error:", err);
+        return res.status(403).json("Error with token");
+      } else {
+        console.log("Decoded Token:", decoded);
+        if (decoded.role === "admin") {
+          next();
+        } else {
+          return res.status(403).json("Not admin");
+        }
+      }
+    });
+  }
+};
+app.get("/dashboard", verifyUser, (req, res) => {
+  res.json("Success");
+});
 app.post("/Login", (req, res) => {
   const { email, password } = req.body;
   UserModel.findOne({ email: email }).then((user) => {
@@ -48,7 +72,7 @@ app.post("/register", (req, res) => {
     .hash(password, 10)
     .then((hash) => {
       UserModel.create({ name, email, password: hash })
-        .then((user) => res.json("success "))
+        .then((user) => res.json("Success"))
         .catch((err) => res.json(err));
     })
     .catch((err) => res.json(err));
@@ -56,4 +80,58 @@ app.post("/register", (req, res) => {
 
 app.listen(3001, () => {
   console.log("server is running");
+});
+
+app.post("/Forgot-Password", (req, res) => {
+  const { email } = req.body;
+  UserModel.findOne({ email: email }).then((user) => {
+    if (!user) {
+      return res.send({ Status: "User not existed" });
+    }
+    const token = jwt.sign({ user: user._id }, "jwt_secret_key", {
+      expiresIn: "1d",
+    });
+
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "sameersethi00001@gmail.com",
+        pass: "tysj mqay izso ucme",
+      },
+    });
+
+    var mailOptions = {
+      from: "sameersethi00001@gmail.com",
+      to: email,
+      subject: "reset your password",
+      text: `http://localhost:5173/Forgot-Password/Reset-password/${user._id}/${token}`,
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        return res.send({ Status: "Success" });
+      }
+    });
+  });
+});
+app.post("/Reset-password/:id/:token", (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+
+  jwt.verify(token, "jwt_secret_key", (err, decoded) => {
+    if (err) {
+      return res.json({ Status: "Error with token " });
+    } else {
+      bcrypt
+        .hash(password, 10)
+        .then((hash) => {
+          UserModel.findByIdAndUpdate({ _id: id }, { password: hash })
+            .then((u) => res.send({ Status: "Success" }))
+            .catch((err) => res.send({ Status: err }));
+        })
+        .catch((err) => res.send({ Status: err }));
+    }
+  });
 });
