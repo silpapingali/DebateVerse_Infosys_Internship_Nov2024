e6c5999ff1e3b075f31505dc844824e8b23cdf4b
BAVESHABIJITH <152059548+baveshabijith07@users.noreply.github.com>
2024-12-12 13:18:08 +0530

    Delete Team3_Debateverse/BAVESHABIJITH directory

--- Team3_Debateverse/BAVESHABIJITH/backend/controllers/Debates.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const Debatelist= (req, res)=>{
-    res.status(200).json({message: "this debatelist route"});
-}
-
-module.exports={Debatelist};
--- Team3_Debateverse/BAVESHABIJITH/backend/routes/debateRoute.js
+++
@@ -1,9 +0,0 @@
-const express= require("express");
-const {Debatelist} = require("../controllers/Debates");
-const {Authjwt} = require("../middlewares/Authjwt");
-
-const DebateRoutes= express.Router();
-
-DebateRoutes.get("/list", Authjwt, Debatelist);
-
-module.exports=  DebateRoutes;
