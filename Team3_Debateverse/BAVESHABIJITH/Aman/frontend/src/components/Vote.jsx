927a0aee2cefb976be6d7301748d6171b08a7c2a
BAVESHABIJITH <baveshabijith.22it@kct.ac.in>
2024-12-12 13:41:00 +0530

    comit

---
+++ Team3_Debateverse/BAVESHABIJITH/Aman/frontend/src/components/Vote.jsx
@@ -0,0 +1,16 @@
+import React, { useRef } from 'react'
+
+const Vote = ({closePopup}) => {
+
+    const popref= useRef();
+
+  return (
+    <div ref={popref} onClick={(e)=> {e.target==popref.current && closePopup()}} className='fixed inset-0 flex justify-center items-center backdrop-blur'>
+        <div className='bg-blue-500 rounded-lg p-10'>
+            <h1>This for voting</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Vote
---
+++ Team3_Debateverse/BAVESHABIJITH/Sanjana/frontend/src/App.jsx
@@ -0,0 +1,46 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserAllDebates from "./pages/UserAllDebates";
+import { Reset } from "./pages/Reset";
+import { UserContextProvider } from "./context/UserContext";
+import About from "./pages/About";
+import UserDashboard from "./pages/UserDashboard";
+import Footer from "./components/Footer";
+import AdminDashboard from "./pages/AdminDashboard";
+import UserProtected from "./ProtectedRoute/UserProtected";
+import AdminProtected from "./ProtectedRoute/AdminProtected";
+import PublicProtected from "./ProtectedRoute/PublicProtected";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <UserContextProvider>
+        <Navbar />
+        <Routes>
+          
+            <Route path="/userdebates" element={<UserAllDebates />} />
+            <Route path="/userdashboard" element={<UserDashboard />} />
+          
+          <Route element={<AdminProtected />}>
+            <Route path="/admindashboard" element={<AdminDashboard />} />
+          </Route>
+
+          <Route element={<PublicProtected />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/aboutus" element={<About />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+          <Route path="/resetpassword" element={<Reset />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <Footer />
+      </UserContextProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
