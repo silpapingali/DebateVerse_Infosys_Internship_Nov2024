08b16b94184320b3caa145b75075e1872bd96e8c
monikaadkine22 <madkine563@gmail.com>
2024-12-15 21:59:28 +0530

    milestone 2

--- Team6_Debateverse/Monika/Debate/frontend/src/App.js
+++ Team6_Debateverse/Monika/Debate/frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import these from react-router-dom
 
+// import { AuthContext, AuthProvider } from './context/AuthContext';
 
 import DebateCard from './components/DebateCard';  // Correct path to the component
 import Navbar from './components/Navbar';  // Correct path to the Navbar component
@@ -10,8 +11,14 @@ import ResetPassword from './pages/ResetPassword';  // Correct path
 import Login from './pages/Login';  // Correct path
 import Signup from './pages/Signup';  // Assuming you have a Signup component
 import AdminDashboard from './components/AdminDashboard';  // Assuming you have AdminDashboard component
-import UserDashboard from './components/UserDashboard';  // Assuming you have UserDashboard component
 import DebateList from './components/DebateList';  // Assuming you have DebateList component
+import CreateDebate from './components/CreateDebate';
+import DebateDetail from './components/DebateDetail'; // Adjust the path as needed
+
+import About from './components/About';
+import Logout from './components/Logout';
+  
+
 
 
 
@@ -32,12 +39,18 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Login/>}></Route>
         <Route path='/signup' element={<Signup/>}></Route>
-        <Route path="/admin-dashboard" element={<AdminDashboard />} />
-        <Route path="/user-dashboard" element={<UserDashboard />} />
+        <Route path="/admindashboard" element={<AdminDashboard />} />
+        <Route path="/debatedetail" element={<DebateDetail />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/verify-otp" element={<VerifyOtp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/debatelist" element={<DebateList />} />
+        <Route path="/createdebate" element={<CreateDebate />} />
+        <Route path="/logout" element={<Logout />} />
+        
+        
+        <Route path="/About" element={<About />} />
+        
         
 
       </Routes>
