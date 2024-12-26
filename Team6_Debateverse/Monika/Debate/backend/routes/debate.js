25d2a998f5cf71e4068205dd5c7b2ce00b2979eb
monikaadkine22 <madkine563@gmail.com>
2024-12-26 20:53:10 +0530

    Add files via upload

--- Team6_Debateverse/Monika/Debate/backend/routes/debate.js
+++ Team6_Debateverse/Monika/Debate/backend/routes/debate.js
@@ -6,16 +6,17 @@ const {
     getDebateDetails,
     allDebates,
     reactions,
-    upvote
-} = require('../controllers/debateController');
+    recentDebates,
+    upvote,
+} = require('../controllers/debateController'); // Ensure correct import
 
 
 DebateRouter.post('/createDebate', createDebate);
 DebateRouter.get('/alldebates', allDebates);
 DebateRouter.get('/reactions', reactions);
-DebateRouter.post('/getDebateDetails', getDebateDetails);
-DebateRouter.get('api/debate/searchDebate', searchDebate);
-DebateRouter.post('/debate/:debateId/option/:optionId/vote', upvote); 
-
+DebateRouter.get('/getDebateDetails', getDebateDetails);
+DebateRouter.get('/searchDebate', searchDebate);
+DebateRouter.get('/debate/:debateId/option/:optionId/vote', upvote); // Ensure this function exists
+DebateRouter.get('/recentDebates', recentDebates);
 
 module.exports = DebateRouter;
