a75595fde5a13a75e5685be4e0900c9bb2846cf2
BAVESHABIJITH <152059548+baveshabijith07@users.noreply.github.com>
2025-01-10 16:32:03 +0530

    Add files via upload

--- Team3_Debateverse/BAVESHABIJITH/backend/controllers/Admin.js
+++ Team3_Debateverse/BAVESHABIJITH/backend/controllers/Admin.js
@@ -1,24 +1,6 @@
 const debatesModel = require("../models/debatesModel");
 const usersModel = require("../models/usersModel");
 
-const AllDebates = async (req, res) => {
-  //   const { userId } = req.user;
-  const { page } = req.query;
-  const skip = (page - 1) * 10;
-  try {
-    const totalRecords = await debatesModel.countDocuments({
-      createdBy: { $ne: createdBy },
-    });
-    const debates = await debatesModel
-      .find({ createdBy: { $ne: createdBy } })
-      .skip(skip)
-      .limit(10)
-      .sort({ createdOn: -1 });
-    res.status(200).json({ totalRecords, debates, likes });
-  } catch (err) {
-    res.status(400).json({ message: "Server error ! Try again later" });
-  }
-};
 const Stats = async (req, res) => {
   try {
     const openDebates = await debatesModel.countDocuments({ status: "open" });
@@ -51,7 +33,7 @@ const fetchUserStatus = async (req, res) => {
 
 const blockUser = async (req, res) => {
   const { userEmail } = req.body;
-  console.log(userEmail);
+  const createdBy = userEmail.split("@")[0];
   try {
     const user = await usersModel.findOneAndUpdate(
       { email: userEmail },
@@ -60,7 +42,8 @@ const blockUser = async (req, res) => {
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }
-    console.log(user);
+
+    await debatesModel.updateMany({ createdBy }, { status: "closed" });
     res.status(200).json({ message: "User blocked !" });
   } catch (err) {
     res.status(400).json({ message: "Server error ! Try again later" });
@@ -69,6 +52,7 @@ const blockUser = async (req, res) => {
 
 const activateUser = async (req, res) => {
   const { userEmail } = req.body;
+  const createdBy = userEmail.split("@")[0];
   try {
     const user = await usersModel.findOneAndUpdate(
       { email: userEmail },
@@ -77,6 +61,7 @@ const activateUser = async (req, res) => {
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }
+    await debatesModel.updateMany({ createdBy }, { status: "open" });
     res.status(200).json({ message: "User activated !" });
   } catch (err) {
     res.status(400).json({ message: "Server error ! Try again later" });
@@ -84,19 +69,60 @@ const activateUser = async (req, res) => {
 };
 
 const fetchAllUsers = async (req, res) => {
+  const { searchQuery = "" } = req.query;
   try {
-    const users = await userModel.find();
+    let query = { role: { $ne: "admin" } };
+    if (searchQuery) {
+      query.email = { $regex: `^${searchQuery}`, $options: "i" };
+    }
+    const users = await usersModel.find(query, { status: 1, email: 1 });
     res.status(200).json(users);
   } catch (error) {
-    res.status(500).json({ message: "Error fetching users", error });
+    res.status(500).json({ message: "Error fetching users !" });
+  }
+};
+
+const closedDebate = async (req, res) => {
+  const { debateId, status } = req.body;
+  try {
+    const debate = await debatesModel.findByIdAndUpdate(
+      debateId,
+      { status },
+      { new: true }
+    );
+    if (!debate) {
+      return res.status(404).json({ message: "Debate not found !" });
+    }
+    res.status(200).json({ message: "Debate successfully Closed !" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error ! Please try again later." });
+  }
+};
+
+const removeOption = async (req, res) => {
+  const { debateId, idx } = req.body;
+  console.log(idx);
+  try {
+    const debate = await debatesModel.findById(debateId);
+    if (!debate || !debate.options || !debate.options[idx]) {
+      return res.status(404).json({ message: "Debate or option not found" });
+    }
+    debate.options[idx].isRemoved = !debate.options[idx].isRemoved;
+    await debate.save();
+    res
+      .status(200)
+      .json({ message: "Option status toggled successfully", debate });
+  } catch (error) {
+    res.status(500).json({ message: "Error toggling option status", error });
   }
 };
 
 module.exports = {
-  AllDebates,
   Stats,
   fetchUserStatus,
   blockUser,
   activateUser,
-  fetchAllUsers
+  fetchAllUsers,
+  closedDebate,
+  removeOption,
 };
